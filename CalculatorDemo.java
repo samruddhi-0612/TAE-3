ec0b1ffea23939d7d357c622a25bf14ae51a7dc0
Samruddhi Sunil Sur <samruddhisur08@gmail.com>
2026-03-27 08:24:11 +0530

    Add files via upload

---
+++ CalculatorDemo.java
@@ -0,0 +1,28 @@
+class Calculator {
+
+    // Two integers
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    // Three integers
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    // Two doubles
+    double add(double a, double b) {
+        return a + b;
+    }
+}
+
+public class CalculatorDemo {
+    public static void main(String[] args) {
+
+        Calculator calc = new Calculator();
+
+        System.out.println("Sum of 2 integers: " + calc.add(5, 10));
+        System.out.println("Sum of 3 integers: " + calc.add(5, 10, 15));
+        System.out.println("Sum of 2 doubles: " + calc.add(5.5, 10.5));
+    }
+}
