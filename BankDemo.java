0bf1a4d4e410c8c8db61ef8e6a0b59cc6fa2ed24
Samruddhi Sunil Sur <samruddhisur08@gmail.com>
2026-03-28 19:55:00 +0530

    Add files via upload

---
+++ BankDemo.java
@@ -0,0 +1,34 @@
+interface Bank {
+    double getInterestRate();
+}
+
+class SBI implements Bank {
+    public double getInterestRate() {
+        return 6.5;
+    }
+}
+
+class HDFC implements Bank {
+    public double getInterestRate() {
+        return 7.0;
+    }
+}
+
+class ICICI implements Bank {
+    public double getInterestRate() {
+        return 6.8;
+    }
+}
+
+public class BankDemo {
+    public static void main(String[] args) {
+
+        Bank b1 = new SBI();
+        Bank b2 = new HDFC();
+        Bank b3 = new ICICI();
+
+        System.out.println("SBI Interest Rate: " + b1.getInterestRate());
+        System.out.println("HDFC Interest Rate: " + b2.getInterestRate());
+        System.out.println("ICICI Interest Rate: " + b3.getInterestRate());
+    }
+}
