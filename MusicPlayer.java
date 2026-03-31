67f4dc69feaa191125de6ffd7c02313a9d951aae
Samruddhi Sunil Sur <samruddhisur08@gmail.com>
2026-03-31 20:07:48 +0530

    Add files via upload

---
+++ MusicPlayer.java
@@ -0,0 +1,18 @@
+class MusicPlayer extends Thread {
+
+    public void run() {
+        System.out.println("Playing song...");
+
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("Progress " + i);
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {}
+        }
+    }
+
+    public static void main(String[] args) {
+        MusicPlayer m = new MusicPlayer();
+        m.start();
+    }
+}
